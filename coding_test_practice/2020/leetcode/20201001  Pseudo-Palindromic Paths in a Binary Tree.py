b9355da873153af7277016d9f253d6d96f8bbe48
weejw <33684393+weejw@users.noreply.github.com>
2020-10-01 17:12:37 +0900

    Update 20201001  Pseudo-Palindromic Paths in a Binary Tree.py

--- coding_test_practice/2020/leetcode/20201001  Pseudo-Palindromic Paths in a Binary Tree.py
+++ coding_test_practice/2020/leetcode/20201001  Pseudo-Palindromic Paths in a Binary Tree.py
@@ -9,15 +9,14 @@ class Solution:
     def find(self, root:TreeNode, route:List) -> bool:
         #마지막 노드일때 지금까지 담은 경로를 확인한다. 이때 모든 요소가 짝수가 아니면 빈 배열을 return
         route = route^{root.val} #★☆○☆★ 가운데 하나빼고 다 짝수여야함 또는 그냥 다 짝수
-        flag = False
         
         if root.left:
-            self.find(root.left, route, cnt)
+            self.find(root.left, route)
         if root.right:
-            self.find(root.right, route, cnt)
-        if not root.left and not root.right: #마지막이니 count
-            if len(route)<2:
-                self.cnt+=1
+            self.find(root.right, route)
+        if not root.left and not root.right and len(route) <2 : #마지막이니 count
+            self.cnt+=1
+            
     def pseudoPalindromicPaths (self, root: TreeNode) -> int:
         #find 함수돌려서 배열이 비어있지 않으면 counting
         self.find(root, set())
