f72dbc341e2cb6b502db690c15d14b65160e97cc
weejw <33684393+weejw@users.noreply.github.com>
2020-10-18 21:24:05 +0900

    Update 20201018 큰 수만들기.py

--- coding_test_practice/2020/프로그래머스/20201018 큰 수만들기.py
+++ coding_test_practice/2020/프로그래머스/20201018 큰 수만들기.py
@@ -2,7 +2,7 @@ def solution(number, k):
     res = []
     for num in number:
         while res and res[-1] < num and k>0:
-            res.pop()
+            res.pop() # pop 말고 slice했을 때는 시간 초과가 났다. 
             k -=1
         res.append(num)
         
